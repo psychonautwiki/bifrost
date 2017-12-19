cae119cdcc45166a68c6c63cb4918f1774876753
Kenan Sulayman <kenan@sly.mn>
2017-12-19 04:22:42 +0100

    Enhance default demo-query to showcase new ROA collection format

--- server/services/graph.js
+++ server/services/graph.js
@@ -44,21 +44,42 @@ module.exports = function* ({app, log}) {
         endpointURL: '/',
         query:
 `{
-  substances(query: "Armodafinil") {
-    effects {
-      name
-      substances {
-        addictionPotential
-        crossTolerances
-        featured
+    substances(query: "Armodafinil") {
         name
-        summary
-        url
-      }
-      url
+
+        # routes of administration
+        roas {
+            name
+
+            dose {
+                units
+                threshold
+                heavy
+                common { min max }
+                light { min max }
+                strong { min max }
+            }
+
+            duration {
+                afterglow { min max units }
+                comeup { min max units }
+                duration { min max units }
+                offset { min max units }
+                onset { min max units }
+                peak { min max units }
+                total { min max units }
+            }
+
+            bioavailability {
+                min max
+            }
+        }
+
+        # subjective effects
+        effects {
+            name url
+        }
     }
-    name
-  }
 }`,
     }));
 
